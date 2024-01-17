e17f19a44d73770936461b0eeb82a1e35df9206d
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2024-01-18 02:32:27 +0700

    first name

--- js/index.js
+++ js/index.js
@@ -1,49 +1,54 @@
 jQuery(document).ready(function ($) {
-
-  $('#checkbox').change(function(){
-    setInterval(function () {
-        moveRight();
+    var intervalId = setInterval(function () {
+      moveRight();
     }, 3000);
-  });
   
     var slideCount = $('#slider ul li').length;
     var slideWidth = $('#slider ul li').width();
     var slideHeight = $('#slider ul li').height();
     var sliderUlWidth = slideCount * slideWidth;
-    
+  
     $('#slider').css({ width: slideWidth, height: slideHeight });
-    
-    $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-    
+  
+    $('#slider ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
+  
     $('#slider ul li:last-child').prependTo('#slider ul');
   
     function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
+      $('#slider ul').animate(
+        {
+          left: +slideWidth,
+        },
+        200,
+        function () {
+          $('#slider ul li:last-child').prependTo('#slider ul');
+          $('#slider ul').css('left', '');
+        }
+      );
+    }
   
     function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
+      $('#slider ul').animate(
+        {
+          left: -slideWidth,
+        },
+        200,
+        function () {
+          $('#slider ul li:first-child').appendTo('#slider ul');
+          $('#slider ul').css('left', '');
+        }
+      );
+    }
   
     $('a.control_prev').click(function () {
-        moveLeft();
+      moveLeft();
     });
   
     $('a.control_next').click(function () {
-        moveRight();
+      moveRight();
     });
+  });
   
-  });    
  
   document.addEventListener("DOMContentLoaded", function() {
       var addToCartButtons = document.querySelectorAll('.product li input[type="submit"]');
